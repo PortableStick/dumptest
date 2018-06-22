595e3bb1ae439c5ecbfdeec2312bf1bae72eda32
PortableStick <crashingwaves@fea.st>
2018-06-22 13:27:19 -0400

    This should fix things

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,21 @@
 import * as React from 'react';
 import './App.css';
 
+import logo from './logo.svg';
+
 class App extends React.Component {
   public render() {
     return (
       <div className="App">
-        Suckit, bitchez!
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.tsx</code> and save to reload.
+        </p>
       </div>
     );
-
-
-
-
-
-
-
-  function        (    ) {};
-
-
-
-  const OK;
-
-
-
-  const uuuuuu = 'this is some ugly shit'
   }
 }
 
