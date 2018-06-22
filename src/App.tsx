f19ecaa7a1c82ea1ac49da5e54129e1664626dba
PortableStick <crashingwaves@fea.st>
2018-06-22 13:02:24 -0400

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import './App.css';
+
+class App extends React.Component {
+          crap() {{}}
+  public render() {
+    return (
+      <div className="App">
+      </div>
+    );
+  }
+}
+
+export default App;
